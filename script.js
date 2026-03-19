aabb00852f9f96f5ab51625ccaf3ba68eb818770
mfsmann <51967793+mfsmann@users.noreply.github.com>
2026-03-19 17:43:09 -0400

    Update card images in memory game

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
         { name: 'card1', img: 'images/Crunchycat.jpg' },
         { name: 'card1', img: 'images/Crunchycat.jpg' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card2', img: 'images/FBI.jpg' },
+        { name: 'card2', img: 'images/FBI.jpg' },
+        { name: 'card3', img: 'images/Hoseob.jpg' },
+        { name: 'card3', img: 'images/Hoseob.jpg' },
+        { name: 'card4', img: 'images/Jim.jpg' },
+        { name: 'card4', img: 'images/Jim.jpg' },
+        { name: 'card5', img: 'images/mike.jpg' },
+        { name: 'card5', img: 'images/mike.jpg' },
         // ...add more pairs as needed
     ];
 
